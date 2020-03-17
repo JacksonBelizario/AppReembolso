84db4022a73f8fc8b2881da04c407794f54202af
Jackson Belizario <jacksonluizzb@gmail.com>
2020-03-17 15:11:15 -0400

    Models

---
+++ App/App.Web/Models/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Web.Models
+{
+    public class Enums
+    {
+        public enum Categorias { Transporte, Alimentação, Outros }
+    }
+}
---
+++ App/App.Web/Models/Solicitacoes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Solicitacoes
+    {
+        public string Id { get; set; }
+        public DateTime DataDaCompra { get; set; }
+        public DateTime DataDaSolicitacao { get; set; }
+        public double Valor { get; set; }
+        public int Categoria { get; set; }
+        public string Descricao { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        public string Anexo { get; set; }
+
+    }
+}
